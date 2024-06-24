01f067e38110500415f773eeb0e65475c5fe88ff
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-24 22:51:36 +0300

    dataset containing the start inex and end index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 """
-
+This module contains a simple helper function
 """
+
+
+def index_range(page, page_size) -> tuple:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
