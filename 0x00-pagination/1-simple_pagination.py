fdf0d207f4c69727c61df19f5322844f5ff17bf0
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-24 23:48:17 +0300

    indentation error fixed

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,17 +8,6 @@ import math
 from typing import List
 
 
-def index_range(page, page_size) -> tuple:
-    """
-    Returns a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
-
-
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -39,20 +28,31 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
+        """
+        This method creates a simple pagination
+        Returns a page from the dataset
+        Use the CSV file provided
+        use of assert
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        end_index = min(end_index, len(dataset))
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
 
 
-def get_page(page=1, page_size=10):
+def index_range(page, page_size) -> tuple:
     """
-    This method creates a simple pagination
-    Returns a page from the dataset
-    Use the CSV file provided
-    use of assert
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters
     """
-    assert type(page) == int and page > 0
-    assert type(page_size) == int and page_size > 0
-    start_index, end_index = index_range(page, page_size)
-    if start_index < len(self.dataset()):
-        return self.dataset()[start_index:end_index]
-    else:
-        []
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
