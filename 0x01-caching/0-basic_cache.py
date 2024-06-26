625e4fd5312fa456dcc39b2a1369a0ff38a7e003
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-27 00:24:01 +0300

    task

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,6 +11,8 @@ class BasicCache(BaseCaching):
     """ BasicCache defines a caching system without limit """
     def put(self, key, item):
         """Does nothing"""
+        pass
 
     def get(self, key):
         """Method """
+        pass
