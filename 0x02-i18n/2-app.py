178ef018d2a82b535ca74f4b0253fd782290813d
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-02 20:43:33 +0300

    Babel

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -9,6 +9,12 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
 
 babel = Babel(app)
 
@@ -19,3 +25,10 @@ def get_locale():
     Determines the best match with the supported languages
     """
     return request.accept_languages.best_match(['fr', 'en'])
+
+@app.route('/')
+def index():
+    return render_template('2-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
