e57a2ba0721e7284011a651f51a75303146dd388
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-07 21:02:39 +0000

    LRU algoritm - discards the least recently used item that caching that puts and gets

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,7 +2,29 @@
 """ BasicCache module """
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
-    """This class"""
+    """LRUCache defines an LRU caching system"""
+    def __init__(self):
+        """Initializes variables"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        """Gets an item by key"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
