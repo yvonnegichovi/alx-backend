953c6358150b49ffa5e1323ee28cbfcd454285e5
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-07 20:04:16 +0000

    FIFO caching that puts and gets

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 """This module"""
 
-
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
-    """This class"""
+    """FIFOCache defines a FIFO caching system"""
+    def __init__(self):
+        """Initializes"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                print(f"DISCARD: {first_key}")
+                self.cache_data.pop(first_key)
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
