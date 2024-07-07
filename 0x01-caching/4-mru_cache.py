7e1da0d555a4543c0eab4ec51f7359ab2f20d3c3
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-07 21:27:22 +0000

    MRU algoritm - discards the mostly recently used item that caching that puts and gets

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,7 +2,29 @@
 """ BasicCache module """
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
-    """This class"""
+    """MRUCache defines an MRU caching system"""
+    def __init__(self):
+        """Initializes variable"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key, _ = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {last_key}")
+
+    def get(self, key):
+        """Gets an item by key"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
