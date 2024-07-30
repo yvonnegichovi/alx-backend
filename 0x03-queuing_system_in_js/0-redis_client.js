fcc4b3cd27bba0b4a06b08274bc75caad2bdb54c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-30 20:15:45 +0300

    task 0

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,9 @@
-import redis from 'redis';
+import { createClient } from 'redis';
 
-const client = redis.createClient({
-  url: 'redis://127.0.0.1:7764',
-});
+const client = createClient();
 
 client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
+  console.log('Redis client not connected to the server:', err.toString());
 });
 
 client.on('connect', () => {
